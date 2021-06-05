9049fac2809a8b39087b9c44d2b752e8f6a4588e
adekeagle <2726949-adekeagle@users.noreply.gitlab.com>
2021-06-05 16:36:52 +0200

    #1 added simple application typed in C#

---
+++ task1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Podaj swoje imię :");
+                var userFirstName = GetNotEmptyString();
+
+                Console.WriteLine("Podaj rok urodzenia :");
+                var yearOfBirth = GetInput();
+
+                Console.WriteLine("Podaj miesiąc urodzenia : ");
+                var monthOfBirth = GetInput();
+
+                Console.WriteLine("Podaj dzień urodzenia : ");
+                var dayOfBirth = GetInput();
+
+                Console.WriteLine("Podaj miejsce urodzenia : ");
+                var placeOfBirth = GetNotEmptyString();
+
+                var dateOfBirth = new DateTime(yearOfBirth, monthOfBirth, dayOfBirth);
+
+                User user = new User(userFirstName, dateOfBirth, placeOfBirth);
+                Console.WriteLine(user.getInfo());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+
+        private static string GetNotEmptyString()
+        {
+            string input = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                throw new Exception("Wartość userFirstName nie może być pusta");
+            }
+
+            return input;
+        }
+
+        private static int GetInput()
+        {
+            if(!int.TryParse(Console.ReadLine(), out int input))
+            {
+                throw new Exception("Podana wartość jest nieprawidłowa");
+            }
+
+            return input;
+        }
+
+
+    }
+}
---
+++ task1/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class User
+    {
+        private string firstname;
+        private DateTime birthday;
+        private string birthplace;
+
+        public User(string firstname, DateTime birthday, string birthplace)
+        {
+            this.firstname = firstname;
+            this.birthday = birthday;
+            this.birthplace = birthplace;
+        }
+
+        private int calculateAge()
+        {
+            return DateTime.Now.Year - birthday.Year;
+        }
+
+        public string getInfo()
+        {
+            return $"Cześć {firstname} urodziłeś się w {birthplace} i masz {calculateAge()} lat";
+        }
+    }
+}
